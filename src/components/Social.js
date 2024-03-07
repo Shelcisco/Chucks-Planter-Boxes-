2b679ef082297390b7036149538fb3a2cfaa3a01
Shelcisco <119394650+Shelcisco@users.noreply.github.com>
2024-03-06 22:24:55 -0800

    Add Links to Social Icons

--- src/components/Social.js
+++ src/components/Social.js
@@ -4,13 +4,13 @@ import '@fortawesome/fontawesome-free/css/all.css';
 const Social = () => {
   return (
     <div className="social-icons">
-      <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="social-icon">
+      <a href="https://www.facebook.com/ChucksPlanterBoxes" target="_blank" rel="noopener noreferrer" className="social-icon">
         <i className="fab fa-facebook-f"></i>
       </a>
-      <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="social-icon">
+      <a href="https://www.instagram.com/chucksplanterboxes/" target="_blank" rel="noopener noreferrer" className="social-icon">
         <i className="fab fa-instagram"></i>
       </a>
-      <a href="https://www.tiktok.com" target="_blank" rel="noopener noreferrer" className="social-icon">
+      <a href="https://www.tiktok.com/@chucksplanterboxes?lang=en" target="_blank" rel="noopener noreferrer" className="social-icon">
         <i className="fab fa-tiktok"></i>
       </a>
     </div>
