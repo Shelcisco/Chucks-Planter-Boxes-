41fb8813260629905442d7d12cb066f494a79863
Shelcisco <119394650+Shelcisco@users.noreply.github.com>
2024-05-05 23:36:39 -0700

    Remove imports no longer needed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "./Nav";
 import Logo from "../assets/Logo.png";
-import Footer from "./Footer";
 import Stencil from './Stencil';
 import Contact from "./Contact";
 import Social from "./Social";
@@ -14,11 +13,12 @@ function Home() {
   return (
   
     <div className="hp">
-
 <Navbar></Navbar>
 <Social></Social>
 <div id="home" className="home">
+
 <img src={Logo} width={500} height={500} alt="Logo"></img>
+
 {/* <Footer></Footer> */}
 </div>
 
