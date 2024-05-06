6b774ab7065b045b260034cdd565f45819d2fc33
Shelcisco <119394650+Shelcisco@users.noreply.github.com>
2024-05-05 23:35:12 -0700

    ADD additional photos

--- src/components/Fullgall.js
+++ src/components/Fullgall.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import Social from "./Social";
+
+//Photos/Videos
 import Parade from "../assets/Parade.mp4";
 import Sasvid from "../assets/Sasvid.mp4";
 import Depth from "../assets/12Deep.mp4";
@@ -18,6 +21,19 @@ import P10 from "../assets/P10.png";
 import P11 from "../assets/P11.png";
 import P12 from "../assets/P12.png";
 import P13 from "../assets/P13.png";
+import P14 from "../assets/P14.png";
+import P15 from "../assets/P15.png";
+import P16 from "../assets/P16.png";
+import P17 from "../assets/P17.png";
+import P18 from "../assets/P18.png";
+import P19 from "../assets/P19.png";
+import P20 from "../assets/P20.png";
+import P21 from "../assets/P21.png";
+import P22 from "../assets/P22.png";
+import P23 from "../assets/P23.png";
+import P24 from "../assets/P24.png";
+import P25 from "../assets/P25.png";
+import Cork from "../assets/Cork.png";
 
 
 
@@ -29,25 +45,44 @@ const Fullgall = () => {
 
   // Sample gallery data
   const sampleGallery = [
-    { id: 1, type: "photo", url: P1, tier: 1 },
-    { id: 2, type: "photo", url: P2, tier: 1 },
-    { id: 3, type: "photo", url: P3, tier: 1 },
-    { id: 4, type: "photo", url: P4, tier: 1 },
-    { id: 5, type: "photo", url: P5, tier: 1 },
-    { id: 6, type: "photo", url: P6, tier: 1 },
-    { id: 7, type: "photo", url: P7, tier: 1 },
-    { id: 8, type: "photo", url: P8, tier: 1 },
-    { id: 9, type: "photo", url: P9, tier: 1 },
-    { id: 10, type: "photo", url: P10, tier: 1 },
-    { id: 11, type: "photo", url: P11, tier: 1 },
-    { id: 12, type: "photo", url: P12, tier: 1 },
-    { id: 13, type: "photo", url: P13, tier: 1 },
+    { id: 22, type: "photo", url: P22},
+    
+    { id: 2, type: "photo", url: P2},
+    { id: 8, type: "photo", url: P8},
+    { id: 3, type: "photo", url: P3},
+    { id: 4, type: "photo", url: P4},
+    
+    { id: 5, type: "photo", url: P5},
+    { id: 17, type: "photo", url: P17},
+    { id: 18, type: "photo", url: P18},
+    { id: 6, type: "photo", url: P6},
+    { id: 7, type: "photo", url: P7},
+    
+    
+    { id: 9, type: "photo", url: P9},
+    { id: 10, type: "photo", url: P10},
+    { id: 11, type: "photo", url: P11},
+    { id: 12, type: "photo", url: P12},
+    
+    { id: 13, type: "photo", url: P13},
+    { id: 14, type: "photo", url: P14},
+    { id: 15, type: "photo", url: P15},
+    { id: 16, type: "photo", url: P16},
+   
+    
+    { id: 19, type: "photo", url: P19},
+    { id: 20, type: "photo", url: P20},
+    { id: 21, type: "photo", url: P21},
+    { id: 1, type: "photo", url: P1},
+    { id: 23, type: "photo", url: P23},
+    { id: 24, type: "photo", url: P24},
+    { id: 25, type: "photo", url: P25},
+    { id: 104, type: "video", url: Crown},
     // Add more items here
-    { id: 101, type: "video", url: Parade, tier: 2 },
-    { id: 102, type: "video", url: Sasvid, tier: 2 },
-    { id: 103, type: "video", url: Flamingo, tier: 2 },
-    { id: 104, type: "video", url: Crown, tier: 2 },
-    { id: 105, type: "video", url: Depth, tier: 2 }
+    { id: 103, type: "video", url: Flamingo},
+    { id: 102, type: "video", url: Sasvid},
+    { id: 101, type: "video", url: Parade},
+    { id: 105, type: "video", url: Depth}
   ];
 
   // Load gallery items on component mount
@@ -62,14 +97,9 @@ const Fullgall = () => {
     // Logic to enlarge item
   };
 
-  // Function to sort items by tier or video
-  const handleSort = (type) => {
-    let sorted = [];
-    if (type === "video") {
-      sorted = galleryItems.filter((item) => item.type === "video");
-    } else {
-      sorted = galleryItems.filter((item) => item.type === "photo");
-    }
+   // Function to sort items by type (photo or video)
+   const handleSort = (type) => {
+    const sorted = galleryItems.filter((item) => item.type === type);
     setSortedItems(sorted);
   };
 
@@ -78,8 +108,14 @@ const Fullgall = () => {
     setSortedItems(galleryItems);
   };
 
+   // Function to close the modal
+   const handleCloseModal = () => {
+    setSelectedItem(null);
+  };
+
   return (
     <div className="fg">
+      <Social></Social>
       <div className="topnav">
         <Link to="/home">Home</Link>
       </div>
@@ -103,10 +139,24 @@ const Fullgall = () => {
           </div>
         ))}
       </div>
-      {/* Modal or enlarged view logic here */}
+{/* Modal */}
+{selectedItem && (
+  <div className="modal">
+    <div className="modal-content">
+      {selectedItem.type === "photo" ? (
+        <img src={selectedItem.url} alt={selectedItem.id} />
+      ) : (
+        <video src={selectedItem.url} controls />
+      )}
+      {/* Place "X" icon in the top-right corner of the white border */}
+      <button className="close-button" onClick={handleCloseModal}>X</button>
+    </div>
+  </div>
+)}
     </div>
   );
 };
+
 
 export default Fullgall;
 
