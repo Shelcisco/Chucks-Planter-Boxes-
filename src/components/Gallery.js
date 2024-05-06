101314f8abe0d849ce2d59ed77a5ae5a22e2854d
Shelcisco <119394650+Shelcisco@users.noreply.github.com>
2024-05-05 23:35:42 -0700

    ADD Videos for Short Gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Link as ScrollLink } from "react-scroll";
 import { Link, useLocation } from "react-router-dom";
-
+import Parade from "../assets/Parade.mp4";
+import Flamingo from "../assets/Flamingo.mp4";
+import Crown from "../assets/Crown.mp4";
 
 export default function Gallery(){
 
@@ -9,19 +11,34 @@ export default function Gallery(){
    const pathname = location.pathname;
 
     return(
-<div className = "shortgal">
-<div className="fgl">
-<Link to="/fullgall" className={pathname === "/fullgall" ? "active" : ""}>
-Click here to see the full Gallery
-</Link>
-  </div>
-<div className="po">Ready to get started? <ScrollLink to="place" smooth={true} duration={500} style={{ color: 'blue' }}>
-   Click here to place your order!
-</ScrollLink> </div>
-
-   </div>
-
-
-
+      <div className="shortgal">
+         <div className= "gtitle">
+         <h1>Gallery</h1>
+         <Link to="/fullgall" className={pathname === "/fullgall" ? "active" : ""}>
+               Click here to see the full Gallery
+            </Link>
+            </div>
+         <div className="sgvid">
+         <video className="video-item" controls>
+               <source src={Flamingo} type="video/mp4" />
+               Your browser does not support the video tag.
+            </video>
+            <video className="video-item" controls>
+               <source src={Parade} type="video/mp4" />
+               Your browser does not support the video tag.
+            </video>
+            <video className="video-item" controls>
+               <source src={Crown} type="video/mp4" />
+               Your browser does not support the video tag.
+            </video>
+         </div>
+         <div className="fgl">
+           </div>
+         <div className="po">
+            Ready to get started? <ScrollLink to="place" smooth={true} duration={500} style={{ color: 'blue' }}>
+               Click here to place your order!
+            </ScrollLink>
+         </div>
+      </div>
     )
 }
